8ea4e26d1aeea7d8812aba2e576900ffd89d37e7
Matus Baniar <matus.baniar@gmail.com>
2021-12-29 18:16:40 +0100

    move logging config to module

--- bayes_ab_test/utilities/logging.py
+++ bayes_ab_test/utilities/logging.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 from os import path
 
-log_file_path = path.join(path.dirname(path.abspath(__file__)), "../../config/logging.conf")
+log_file_path = path.join(path.dirname(path.abspath(__file__)), "logging.conf")
 
 logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
 
